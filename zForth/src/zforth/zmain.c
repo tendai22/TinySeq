86237d17a3e9233f5ba8873b7a42a33032a79ae3
Norihiro Kumagai <tendai22plus@gmail.com>
2022-08-20 06:44:54 +0900

    a forgotten zmain.c file added

---
+++ zForth/src/zforth/zmain.c
@@ -0,0 +1,269 @@
+//
+//
+//
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include "zforth.h"
+
+#ifdef USE_STDIO
+#include <stdio.h>
+#endif //USE_STDIO
+#ifdef USE_XPRINTF
+#include "xprintf.h"
+#endif //USE_XPRINTF
+
+//
+// extern declaration
+//
+extern const char zf_src_core_file[];
+
+//
+// xprintf support functions
+//
+#ifdef USE_XPRINTF
+
+void (*xfunc_output)(int) = _mon_putc;
+
+
+void vdprintf(const char *fmt, va_list arp)
+{
+	xvfprintf(_mon_putc, fmt, arp);
+}
+
+void dprintf(const char *fmt, ...)
+{
+	va_list arp;
+	va_start(arp, fmt);
+	xvfprintf(_mon_putc, fmt, arp);
+	va_end(arp);
+}
+#endif // USE_XPRINTF
+
+/*
+ * Evaluate buffer with code, check return value and report errors
+ */
+
+zf_result do_eval(const char *src, int line, const char *buf)
+{
+	const char *msg = NULL;
+
+	zf_result rv = zf_eval(buf);
+
+	switch(rv)
+	{
+		case ZF_OK: break;
+		case ZF_ABORT_INTERNAL_ERROR: msg = "internal error"; break;
+		case ZF_ABORT_OUTSIDE_MEM: msg = "outside memory"; break;
+		case ZF_ABORT_DSTACK_OVERRUN: msg = "dstack overrun"; break;
+		case ZF_ABORT_DSTACK_UNDERRUN: msg = "dstack underrun"; break;
+		case ZF_ABORT_RSTACK_OVERRUN: msg = "rstack overrun"; break;
+		case ZF_ABORT_RSTACK_UNDERRUN: msg = "rstack underrun"; break;
+		case ZF_ABORT_NOT_A_WORD: msg = "not a word"; break;
+		case ZF_ABORT_COMPILE_ONLY_WORD: msg = "compile-only word"; break;
+		case ZF_ABORT_INVALID_SIZE: msg = "invalid size"; break;
+		case ZF_ABORT_DIVISION_BY_ZERO: msg = "division by zero"; break;
+		default: msg = "unknown error";
+	}
+
+	if(msg) {
+		dprintf("\033[31m");
+		if(src) dprintf("%s:%d: ", src, line);
+		dprintf("%s\033[0m\n", msg);
+	}
+
+	return rv;
+}
+
+//
+// include from an embedded character array (which holds a source code)
+//
+
+void include_str(const char *fname, const char *str)
+{
+	char buf[256];
+	const char *src = str;
+	char *dest = buf;
+	int c, line = 1;
+	while ((c = *src++) != 0) {
+		if (c == '\r')
+			continue; 	// ignore carridge return
+		if (c != '\n') {
+			*dest++ = c;
+			continue;
+		}
+		// end of line
+		*dest = '\0';
+		dest = buf;
+		//dprintf ("%s:%d:%s\r\n", fname, line, buf);
+		do_eval(fname, line++, buf);
+	}
+}
+
+/*
+ * Sys callback function
+ */
+
+zf_input_state zf_host_sys(zf_syscall_id id, const char *input)
+{
+	switch((int)id) {
+
+		/* The core system callbacks */
+
+		case ZF_SYSCALL_EMIT:
+			xputc((char)zf_pop());
+			break;
+
+		case ZF_SYSCALL_PRINT:
+			xprintf(ZF_CELL_FMT " ", zf_pop());
+			break;
+
+		case ZF_SYSCALL_TELL: {
+				zf_cell len = zf_pop();
+				void *buf = (uint8_t *)zf_dump(NULL) + (int)zf_pop();
+				for (char *p = (char *)buf; len-- > 0; ) {
+					xputc(*p++);
+				}
+		    }
+			break;
+		/* Application specific callbacks */
+
+		case ZF_SYSCALL_USER + 0:
+			xprintf("\r\n");
+			exit(0);
+			break;
+
+		case ZF_SYSCALL_USER + 1:
+			//zf_push(sin(zf_pop()));
+			break;
+#ifdef ZF_USE_FILE
+		case ZF_SYSCALL_USER + 2:
+			if(input == NULL) {
+				return ZF_INPUT_PASS_WORD;
+			}
+			include(input);
+			break;
+		
+		case ZF_SYSCALL_USER + 3:
+			save("zforth.save");
+			break;
+#endif //ZF_USE_FILE
+		default:
+			xprintf("unhandled syscall %d\r\n", id);
+			break;
+	}
+
+	return ZF_INPUT_INTERPRET;
+}
+
+
+/*
+ * Tracing output
+ */
+
+void zf_host_trace(const char *fmt, va_list va)
+{
+	dprintf("\033[1;30m");
+	vdprintf(fmt, va);
+	dprintf("\033[0m");
+}
+
+
+/*
+ * Parse number
+ */
+
+zf_cell zf_host_parse_num(const char *buf)
+{
+	zf_cell v;
+	int n = 0;
+	const char *p = buf;
+	int r = xatoi(&p, &v);
+	n = p - buf;
+	if(r == 0 || buf[n] != '\0') {
+		zf_abort(ZF_ABORT_NOT_A_WORD);
+	}
+	return v;
+}
+
+void usage(void)
+{
+	dprintf( 
+		"usage: zfort [options] [src ...]\r\n"
+		"\r\n"
+		"Options:\r\n"
+		"   -h         show help\r\n"
+		"   -t         enable tracing\r\n"
+		"   -l FILE    load dictionary from FILE\r\n"
+	);
+}
+
+//
+//
+//
+
+int zf_fgets(char *buf, size_t len/*, FILE *fp*/)
+{
+	size_t i;
+    int c;
+    for (i = 0; i < len ; ) {
+        while ((c = _mon_getc()) == -1)
+            ;
+        if (i >= len - 1 || c == '\r' || c == '\n') {
+            buf[i] = '\0';
+            _mon_putc('\r');
+            _mon_putc('\n');
+            break;
+        }
+        buf[i++] = c;
+        _mon_putc(c);
+    }
+    return (int)i;
+}
+
+/*
+ * Main
+ */
+
+int zmain(int argc, char **argv)
+{
+//	int i;
+	int trace = 0;
+	int line = 0;
+
+	/* Initialize zforth */
+
+	zf_init(trace);
+
+
+	/* Load dict from disk if requested, otherwise bootstrap fort
+	 * dictionary */
+
+	zf_bootstrap();
+
+	/* load core.zf array
+	 */
+
+	include_str("core.zf", &zf_src_core_file[0]);
+
+	/*
+	 * interactive interpreter
+	 */
+	while (1) {
+		char buf[256];
+		if(zf_fgets(buf, sizeof(buf)/*, stdin*/)) {
+			do_eval("conin", ++line, buf);
+		} else {
+			break;
+		}
+	}
+
+	return 0;
+}
+
+
+/*
+ * End
+ */
